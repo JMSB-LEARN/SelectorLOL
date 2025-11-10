02e57f0145fdd1a04f5813f0083e01275118e970
JMSB-LEARN <jmarquess04@iesalbarregas.es>
2025-11-10 16:01:23 +0000

    start

---
+++ src/app/champion-detail-component/champion-detail-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-champion-detail-component',
+  imports: [],
+  templateUrl: './champion-detail-component.html',
+  styleUrl: './champion-detail-component.css',
+})
+export class ChampionDetailComponent {
+
+}
---
+++ src/app/champion-list-component/champion-list-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-champion-list-component',
+  imports: [],
+  templateUrl: './champion-list-component.html',
+  styleUrl: './champion-list-component.css',
+})
+export class ChampionListComponent {
+
+}
