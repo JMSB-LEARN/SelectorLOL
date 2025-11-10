60977d24bc9bb7a03c91abdea19500d0f5131393
JMSB-LEARN <jmarquess04@iesalbarregas.es>
2025-11-10 16:09:25 +0000

    parent Componets Created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChampionListComponent } from "./champion-list-component/champion-list-component";
+import { ChampionDetailComponent } from "./champion-detail-component/champion-detail-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChampionListComponent, ChampionDetailComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
